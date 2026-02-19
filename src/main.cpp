7a2af78ba538db0c1dc41d8c5888ac083c495738
Nathanael-Tekalign <156130272+Nathanael-Tekalign@users.noreply.github.com>
2026-02-18 20:30:36 -0600

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "AppDelegate.h"
+#include "base/CCDirector.h"
+#include "platform/CCApplication.h"
+USING_NS_CC;
+
+int main(int argc, char** argv) {
+    AppDelegate app;
+    return Application::getInstance()->run();
+}
